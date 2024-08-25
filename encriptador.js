bf948e0ac522dcc318bd79d7d643e6e210d25176
Leobjj <leoadiestrador@gmail.com>
2024-08-24 22:16:33 -0400

    Add files via upload

---
+++ encriptador.js
@@ -0,0 +1,29 @@
+function encriptarTexto() {
+  let encriptarTexto = document.getElementById("cajaTexto1").value; 
+  let textoEncriptado = encriptarTexto.replace(/a/g, "ai")
+                                      .replace(/e/g, "enter")
+                                      .replace(/i/g, "imes")
+                                      .replace(/o/g, "ober")
+                                      .replace(/u/g, "ufat");
+
+  console.log(textoEncriptado);
+  cajaTextoResultado.value = textoEncriptado;
+  let resultado = document.getElementById("cajaTextoResultado");
+}
+
+function descifrarTexto() {
+
+   let textoEncriptado = document.getElementById("cajaTexto1").value; 
+
+  let descifrarTexto = textoEncriptado.value; 
+  let textoDescifrado = textoEncriptado.replace(/enter/g, "e") 
+                                      .replace(/imes/g, "i") 
+                                      .replace(/ober/g, "o") 
+                                      .replace(/ufat/g, "u") 
+                                      .replace(/ai/g, "a");
+
+  console.log(textoDescifrado);
+  cajaTextoResultado.value = textoDescifrado;
+  let resultado = document.getElementById("cajaTextoResultado");
+}
+
